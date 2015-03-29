0401d556a4f875004df6cac3fabc856eadb43e9b
Timothy Yeh <timothy.yeh@gmail.com>
2015-03-29 05:26:38 -0700

    Initial Commit

---
+++ FavoritePhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FavoritePhotos
+//
+//  Created by tim on 3/29/15.
+//  Copyright (c) 2015 Timothy Yeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
